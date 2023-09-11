2640b962dc31d1561c9c065e031a424d3043b2c5
Megan Venetianer <megan.venetianer@gmail.com>
2023-09-10 21:47:38 -0400

    update form styling

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,7 +1,8 @@
 <template>
+  <v-img height="100" src="@/assets/Paradigm_logo.png" />
   <v-form ref="form" @submit.prevent>
-    <v-container>
-      <v-col cols="12" md="4" pad>
+    <v-container class="d-flex justify-center">
+      <v-col cols="10" md="4">
         <v-text-field
           v-model="email"
           :rules="emailRules"
@@ -18,6 +19,7 @@
         <v-text-field
           v-model="confirmedPassword"
           :rules="confirmedPasswordRules"
+          type="password"
           label="Confirm Password"
           required
         ></v-text-field>
@@ -25,7 +27,9 @@
           >Submit
         </v-btn>
         <div>
-          {{ errorMessage }}
+          <p :class="[error ? 'error-message' : 'submit-success-message']">
+            {{ feedbackMessage }}
+          </p>
         </div>
       </v-col>
     </v-container>
@@ -38,8 +42,9 @@ import { writeUserData, getUserData, encryptPassword } from "../firebase/init";
 export default {
   data: () => ({
     password: "",
-    errorMessage: "",
+    feedbackMessage: "",
     confirmedPassword: "",
+    error: false,
     passwordRules: [
       (v) => (v && v.length >= 6) || "Password must be at least 6 characters",
     ],
@@ -59,27 +64,31 @@ export default {
         if (valid) {
           const data = await getUserData(this.email);
           if (data) {
-            this.errorMessage = "Email already exists";
+            this.feedbackMessage = "Email already exists";
+            this.error = true;
             return;
           } else {
             writeUserData(this.email, encryptPassword(this.password));
-            this.errorMessage = `Submitted email: ${this.email} successfully`;
+            this.feedbackMessage = `Submitted email: ${this.email} successfully`;
             this.resetForm();
           }
         } else {
-          this.errorMessage = "Please fix the above fields";
+          this.feedbackMessage = "Please fix or complete the fields above";
+          this.error = true;
         }
       } catch (error) {
         console.error(error);
-        this.errorMessage =
+        this.feedbackMessage =
           "An error occurred when trying to submit. Please try again.";
+        this.error = true;
       }
     },
     async resetForm() {
       await this.$refs.form.reset();
+      this.error = false;
 
       setTimeout(() => {
-        this.errorMessage = "";
+        this.feedbackMessage = "";
       }, 6000);
     },
   },
@@ -94,4 +103,22 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.submit-success-message {
+  color: #007505;
+  padding: 6px 16px 0px;
+  font-size: 12px;
+  letter-spacing: 0.0333333333em;
+}
+
+.error-message {
+  color: #b00020;
+  padding: 6px 16px 0px;
+  font-size: 12px;
+  letter-spacing: 0.0333333333em;
+}
+
+.v-text-field {
+  padding-top: 6px;
+}
+</style>
